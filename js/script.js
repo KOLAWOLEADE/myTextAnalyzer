4507d54482e5f80e00fbcc0ac52c2d05bc9c9c23
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-09-24 14:23:40 +0100

    add counterArray

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-function wordCounter(counter){
-    if (counter.trim().lenght ===0)
+// BUSINESS LOGIC
 
+//  WORDCOUNTER
+
+function wordCounter(counter){
+    if (counter.trim().lenght ===0){
+
+        return 0;
+    }
+let counter = 0
+const counterArray = counter.split(" ");
+counterArray.forEach(function(element){
+    if (!Number(element)){
+        counter++;
+    }
+
+}) 
+   return counter;
+
+}
+
+
+// Appearance
+
+function NumberOfOccurrencesInText(appearance){
+    if (counter.trim().length === 0);
     return 0;
 }
 
 let counter = 0
 const counterArray = counter.split(" ");
-counterArray.forEach(function(element)){
-    if (!Number(element))
-}
+counterArray.forEach(function(element){
+
+})
+
+
