f10ff6eb41c35e51ba75db819a13d91277db28b8
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-09-24 13:56:44 +0100

    add number

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function wordCounter(counter){
     if (counter.trim().lenght ===0)
 
-    return 0
+    return 0;
+}
+
+let counter = 0
+const counterArray = counter.split(" ");
+counterArray.forEach(function(element)){
+    if (!Number(element))
 }
