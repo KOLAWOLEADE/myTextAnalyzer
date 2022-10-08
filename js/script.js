3ff299c4b2bd9c8686a826e11be10decc8ba5186
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2022-10-08 14:21:29 +0100

    add reapeatedWords

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 function noInputtedWord(word,text){
-    ((word.trim().length === 0) || (text.trim().length === 0))
-    return 0;
+  ((word.trim().length === 0) || (text.trim().length === 0))
+  return 0;
+  }
+
+  // word counter
+function wordCounter(text) {
+    if (text.trim().length === 0) {
+
     }
-  
-    // word counter
-  function wordCounter(text) {
-      if (text.trim().length === 0) {
-  
+    let wordCount = 0;
+    const wordArray = text.split(" ");
+    wordArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
       }
+    });
+    return wordCount;
+  }
 
-      let wordCount = 0;
-      const wordArray = text.split(" ");
-      wordArray.forEach(function(element) {
-        if (!Number(element)) {
-          wordCount++;
-        }
-      });
-      return wordCount;
-    }
-
-
-     // number of occurences
+  
+  // number of occurences
   function numberOfOccurrencesInText(word, text) {
     if (word.trim().length === 0) {
       return 0;
@@ -35,31 +34,52 @@ function noInputtedWord(word,text){
     return wordCount;
   }
 
-
-  //   function to bold text
+//   function to bold text
 function boldPassage(word, text) {
-    if(noInputtedWord(word,text)){
-      return ""
+  if(noInputtedWord(word,text)){
+    return ""
+  }
+    let htmlString = "<p>";
+    let textArray = text.split(" ");
+    textArray.forEach(function(element, index) {
+      if (element.toLowerCase().includes(word.toLowerCase()))  {
+        htmlString = htmlString.concat( ("<b>" + word + "</b>"));
+      } else {
+        htmlString = htmlString.concat(element);
+      }
+      if (index !== (textArray.length - 1)) {
+        htmlString = htmlString.concat(" ");
+      }
+    });
+    return htmlString + "</p>";
+    
+
+  }
+
+   // wordsCounter
+
+  function repeatedWords(text){
+    const chars = [];
+    const arr = text.split(" ");
+  
+    for (let word of arr ) {
+      if (!chars[word]){
+        chars[word] = 1;
+      }
+      else{
+        chars[word]++;
+      }
     }
-      let htmlString = "<p>";
-      let textArray = text.split(" ");
-      textArray.forEach(function(element, index) {
-        if (element.toLowerCase().includes(word.toLowerCase()))  {
-          htmlString = htmlString.concat( ("<b>" + word + "</b>"));
-        } else {
-          htmlString = htmlString.concat(element);
-        }
-        if (index !== (textArray.length - 1)) {
-          htmlString = htmlString.concat(" ");
-        }
-      });
-      return htmlString + "</p>";
-    }
+  
+    console.log(chars);
+   
+
+  }
 
 
-    //UI Logic
+//UI Logic
 $(document).ready(function(){
-    $("form#word-counter").submit(function(event){
+    $("form#myTestAnalyzer").submit(function(event){
       event.preventDefault();
       const text = $("#text-passage").val();
       const word = $("#word").val();
@@ -68,129 +88,23 @@ $(document).ready(function(){
       $("#total-count").html(wordCount);
       $("#selected-count").html(occurrencesOfWord);
       $("#bolded-passage").html(boldPassage(word, text));
+      $("#most").html(chars)
 
 
         // most used words
-  let most = []
-  let usedWords = text.split(" ");
-  usedWords.forEach(function(element){
-    most.push(element);
-  })
-  most.forEach(function(element, index){
-    let str = "<li>"
-    $("#most").append(str + element + " " + most[index])
-  })
+  // let most = [];
+  // let usedWords = text.split(" ");
+  // usedWords.forEach(function(element){
+  //   most.push(element);
+  // })
+  // most.forEach(function(element, index){
+  //   let str = "<li>"
+  //   $("#most").append(str + element + " " + most[index])
+  // })
     })
   });
 
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// BUSINESS LOGIC
-
-// function wordCounter(sentence){
-//     if (sentence.trim().lenght ===0){
-
-//         return 0;
-//     }
-// let counters = 0
-// const counterArray = sentence.split(" ");
-// counterArray.forEach(function(element){
-//     if (!Number(element)){
-//         counters++;
-//     }
-
-// }) 
-//    return counters;
-
-// }
-
-// console.log(element)
-
-// const sentence = "to to  go bum hi lo bum bum Bum"
-// const newArr = []
-// const sentenceArr = sentence.split()
-
-// for ( let word of sentenceArr) {
-//     if (!newArr[word]) {
-//         newArr[word] = 1;
-//     }
-
-//     else {
-//         newArr[word]++
-    
-//     }
-// }
-
-
-
-
-
-
-
-
-// function NumberOfOccurrencesInSentence(sentence){
-//     if (sentence.trim().length === 0 );{
-
-//         return 0;
-//     }
-
-//     const counterArray = sentence.split(" ");
-//     let counters = 0;
-//     counterArray.forEach(function(element){
-//     if (element > 0) {
-
-//         counters++;
-
-//     }
-    
-// });
-
-// return counters;
-
-// }
-
-
-
-
-// UI LOGIC 
-
-
-// $(document).ready(function(){
-//     $("form#myTestAnalyzer").submit(function(event){
-//       event.preventDefault();
-
-//       const counter = $("#counter").val();
-
-//       $("#highestCount").html(counters)
-
-
-
-
-    //   const passage = $("#text-passage").val();
-    //   const word = $("#word").val();
-    //   const wordCount = wordCounter(passage);
-    //   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-    //   $("#total-count").html(wordCount);
-    //   $("#selected-count").html(occurrencesOfWord);
-    //   $("#bolded-passage").html(boldPassage(word, passage));
-//     });
-//   });
-
-
-
-
-
+ 
+ 
